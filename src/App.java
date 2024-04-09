1dba19f11ad8813745e1f25e45fab3f617e5d8a7
Grt1 <gt22013p@pace.edu>
2024-04-08 21:44:45 -0400

    ff

--- src/App.java
+++ src/App.java
@@ -1,5 +1,42 @@
+import java.util.*;
+import java.io.*;
+
 public class App {
+    public static String userInput;
+    public static ArrayList<String> logs = new ArrayList<String>();
     public static void main(String[] args) throws Exception {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter 1 to log an exercise, 2 to log calories, 3 to log hydration.");
+        int choice = sc.nextInt();
+        if (choice == 1){
+            System.out.println("Please log you exercise in this format 'name, time(in hours), intensity(1-10)': ");
+            while (true) {
+            userInput = sc.nextLine();
+            if(isValid(userInput)){
+                logs.add(userInput);
+                break;
+            } else {
+                System.out.print("Invalid entry for \"" + userInput + "\". Please try again: ");
+                System.out.println("Please log you exercise in this format 'name, time(in hours), intensity(1-10)': ");
+            }
+        }
+        } else if (choice == 2){
+
+        } else if (choice == 3){
+
+        }
+
+
+        sc.close();
+    }   
+    private static boolean isValid(String input) {
+        try{
+        if(Exercise(userInput) == true){
+            return !input.isEmpty();
+        } else {
+            return false;
+        }
+    } catch ()
     }
 }
---
+++ src/Exercise.java
@@ -0,0 +1,23 @@
+public class Exercise {
+    private String exercise;
+    private int hours;
+    private int intensity; //Should be a number 1-10
+    
+    public Exercise(String exercise, int hours, int intensity){
+        this.exercise = exercise;
+        this.hours = hours;
+        this.intensity = intensity;
+    }
+
+    public String getExercise(){
+        return exercise;
+    }
+    public int getHours(){
+        return hours;
+    }
+
+    public int getIntensity(){
+        return intensity;
+    }
+
+}
