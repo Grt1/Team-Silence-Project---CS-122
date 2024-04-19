5c058ea55052fb0136b55e3c6ea630cd2c92c9f4
Alberdi <aj03955p@pace.edu>
2024-04-19 11:23:48 -0400

    Implement Calories

--- src/Calorie.java
+++ src/Calorie.java
@@ -1,3 +1,28 @@
 public class Calorie {
+    private int gained;
+    private int spent;
+    private int total;
     
+    public Calorie(int gained, int spent) {
+        this.gained = gained;
+        this.spent = spent;
+        this.total = gained - spent;
+    }
+
+    public int getGainedCalories() {
+        return gained;
+    }
+
+    public int getSpentCalories() {
+        return spent;
+    }
+
+    public int getTotalCalories() {
+        return total;
+    }
+
+    @Override
+    public String toString(){
+        return gained + ", " + spent + ", " + total;
+    }
 }
