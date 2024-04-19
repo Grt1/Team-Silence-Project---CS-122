a52327faf91a109046d20764982de40a1f560404
Alberdi <aj03955p@pace.edu>
2024-04-19 11:36:01 -0400

    Implementing Calorie

--- src/App.java
+++ src/App.java
@@ -9,13 +9,19 @@ public class App {
     private static ArrayList<Exercise> logs = new ArrayList<>();
     public static JTextArea exerciseLogs = new JTextArea();
 
+    private static ArrayList<Calorie> calorieLogs = new ArrayList<>();
+    public static JTextArea calorieLogsArea = new JTextArea();
+    public static int totalCalories = 0;
+
     public static void main(String[] args) {
         JFrame mainFrame = new JFrame("Team Silence - Fitness Tracker");
         JPanel panel = new JPanel();
         JButton Exercise = new JButton("Workout");
+        JButton Calories = new JButton("Calories");
 
         panel.add(new JLabel("Choose an activity to log"));
         panel.add(Exercise);
+        panel.add(Calories);
         mainFrame.add(panel);
         mainFrame.setSize(300, 150);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -61,10 +67,51 @@ public class App {
             }
         });
 
+        Calories.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame exerciseFrame = new JFrame("Calories");
+                JPanel panel1 = new JPanel();
+                JTextField ExerciceInputHours = new JTextField(20);
+                JTextField ExerciseInputIntensity = new JTextField(20);
+                JButton submitExercise = new JButton("Submit");
+                
+                Box.Filler filler = new Box.Filler(new Dimension(0, 0), new Dimension(0, 40), new Dimension(200, 40));
+                panel.add(filler);
+                panel1.add(new JLabel("Enter the amount of calories gained:"));
+                panel1.add(ExerciceInputHours);
+                panel1.add(new JLabel("Enter the amount of calories spent:"));
+                panel1.add(ExerciseInputIntensity);
+                panel1.add(submitExercise);
+                panel1.add(calorieLogsArea);
+
+                submitExercise.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        int gained = Integer.parseInt(ExerciceInputHours.getText());
+                        int spent = Integer.parseInt(ExerciseInputIntensity.getText());
+                        Calorie calorie = new Calorie(gained, spent);
+                        totalCalories += calorie.getTotalCalories();
+                        calorieLogs.add(calorie);
+                        updateDisplayPanel();
+                    }
+                });
+
+                exerciseFrame.add(panel1);
+                exerciseFrame.setSize(400, 300);
+                exerciseFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                exerciseFrame.setVisible(true);
+            }
+        });
+
         exerciseLogs.setEditable(false);
         exerciseLogs.setLineWrap(true);
         exerciseLogs.setWrapStyleWord(true);
+        calorieLogsArea.setEditable(false);
+        calorieLogsArea.setLineWrap(true);
+        calorieLogsArea.setWrapStyleWord(true);
         panel.add(exerciseLogs);
+        panel.add(calorieLogsArea);
     }
     public ArrayList<Exercise> getLogs(){
         return logs;
@@ -75,7 +122,11 @@ public class App {
         for (Exercise exercise : logs) {
             exerciseLogs.append(exercise.toString() + "\n");
         }
+        calorieLogsArea.setText(""); 
+        for (Calorie calorie : calorieLogs) {
+            calorieLogsArea.append(calorie.toString() + "\n");
+        }
+        calorieLogsArea.append("Total: " + totalCalories);
     }
 
 }
-
