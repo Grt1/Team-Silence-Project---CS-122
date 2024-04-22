d90e2f907264bcc8b2293ed22ca25dab155c11f1
Alexandra <at22918p@pace.edu>
2024-04-22 19:56:18 -0400

    Hydration

--- src/Hydration.java
+++ src/Hydration.java
@@ -1,3 +1,22 @@
 public class Hydration {
-    
+    private String beverage;
+    private double ounces;
+
+    public Hydration(String beverage, double ounces) {
+        this.beverage = beverage;
+        this.ounces = ounces;
+    }
+
+    public String getBeverage() {
+        return beverage;
+    }
+
+    public double getOunces() {
+        return ounces;
+    }
+
+    @Override
+    public String toString() {
+        return beverage + ", " + ounces + " ounces";
+    }
 }
